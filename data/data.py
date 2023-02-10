ddc79c3fc7bea4f4e97171034900fbb345ea15d2
krogers <orkrod98@gmail.com>
2023-02-10 10:17:50 -0700

    add fashionmnist

--- data/data.py
+++ data/data.py
@@ -1,23 +1,36 @@
 import torch
 import torchvision
 
-def get_train_test_loader(batch_size_train = 64, batch_size_test = 1000, path = './data/'):
-    train_loader = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST(path, train=True, download=True,
-                                    transform=torchvision.transforms.Compose([
+def get_train_test_loader(dataset_name, batch_size_train = 64, batch_size_test = 1000, path = './data/'):
+    transform = torchvision.transforms.Compose([
                                     torchvision.transforms.ToTensor(),
                                     torchvision.transforms.Normalize(
                                         (0.1307,), (0.3081,))
-                                    ])),
-    batch_size=batch_size_train, shuffle=True)
+                                    ])
+    supported_datasets = ['mnist','fashionmnist']
+    if dataset_name == 'mnist':
+        train_loader = torch.utils.data.DataLoader(
+            torchvision.datasets.MNIST(path, train=True, download=True,
+                                        transform=transform),
+        batch_size=batch_size_train, shuffle=True)
 
-    test_loader = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST(path, train=False, download=True,
-                                    transform=torchvision.transforms.Compose([
-                                    torchvision.transforms.ToTensor(),
-                                    torchvision.transforms.Normalize(
-                                        (0.1307,), (0.3081,))
-                                    ])),
-    batch_size=batch_size_test, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(
+            torchvision.datasets.MNIST(path, train=False, download=True,
+                                        transform=transform),
+        batch_size=batch_size_test, shuffle=True)
+    elif dataset_name == 'fashionmnist':
+        train_loader = torch.utils.data.DataLoader(
+            torchvision.datasets.FashionMNIST(path, train=True, download=True,
+                                        transform=transform),
+        batch_size=batch_size_train, shuffle=True)
+
+        test_loader = torch.utils.data.DataLoader(
+            torchvision.datasets.FashionMNIST(path, train=False, download=True,
+                                        transform=transform),
+        batch_size=batch_size_test, shuffle=True)
+
+    else:
+        raise NotImplemented(f"Dataset: {dataset_name} not implemented yet. Please supply one of the following as dataset_name, {supported_datasets}")
+    
     return train_loader, test_loader
 
