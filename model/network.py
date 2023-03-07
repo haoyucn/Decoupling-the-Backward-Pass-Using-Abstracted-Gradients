afca5904b1c4c2d876f0450c2c236f933d818466
Hao <haoyucn@hotmail.com>
2023-03-07 13:13:28 -0700

    left over commit

--- model/network.py
+++ model/network.py
@@ -50,21 +50,56 @@ class TeacherNet(nn.Module):
 class MNet(torch.nn.Module):
     def __init__(self, sequentialLayers):
         super(MNet, self).__init__()
-        self.sequentialLayers = sequentialLayers
-        self.sequentialLayers.requires_grad = False
+        self.layers = []
+        for l in sequentialLayers:
+            if not isinstance(l, nn.ReLU):
+                l.requires_grad = False
+                self.layers.append(l)
+        # self.layers.requires_grad = False
+        self.input_x = None
+        self.layersOutput = []
+
+    def getLayersOutput(self, x):
+        self.layersOutput = []
+
+        x_clone = x.clone().detach()
+        x_clone.requires_grad = False
+        self.input_x = x_clone
+
+        lo = x_clone
+        for l in self.layers:
+            lo = l(lo)
+            self.layersOutput.append(lo)
+            lo = F.relu(lo)
+
+        lo = lo.detach()
+        lo.requires_grad = False
+        return lo
+
+    def weightUpdate(self, correctness, lr = 0.001):
+        for i in range(len(self.layersOutput)):
+            output = self.layersOutput[i]
+            layer = self.layers[i]
+            maxIndexs = torch.argmax(output, dim = -1)
+            weightChange = layer.weight.abs() * (1 - layer.weight.abs()) * (-1)
+            for j in range(len(maxIndexs)):
+                if not correctness[j]:
+                    index = maxIndexs[j]
+                    deltaWeight = weightChange.clone()
+                    deltaWeight[int(index)] = deltaWeight[int(index)] * (-1)
+                    layer.weight = torch.nn.Parameter(layer.weight + deltaWeight * lr)
 
     def forward(self, x):
         x_clone = x.clone().detach()
         x_clone.requires_grad = False
-        sequentialOutput = self.sequentialLayers(x_clone)
+        sequentialOutput = self.getLayersOutput(x_clone)
         x_clone_inv = torch.linalg.pinv(x_clone)
         m = F.linear(x_clone_inv, torch.transpose(sequentialOutput, 0, 1)).detach()
         return F.linear(x, torch.transpose(m, 0, 1))
         
 
-
 class FastUpdateNet(torch.nn.Module):
-    def __init__(self, teacherNet = None):
+    def __init__(self, teacherNet = None, total_image_pixel = 784):
         super(FastUpdateNet, self).__init__()
         if teacherNet:
             self.fc1 = teacherNet.fc1
@@ -77,10 +112,7 @@ class FastUpdateNet(torch.nn.Module):
 
     def forward(self, x):
         o_1 = torch.reshape(x, (x.shape[0], 28*28))
-        
         o_2 = F.relu(self.fc1(o_1))
-
         o_3 = self.mNet(o_2)
-
         o_4 = self.fc3(o_3)
         return F.log_softmax(o_4)
