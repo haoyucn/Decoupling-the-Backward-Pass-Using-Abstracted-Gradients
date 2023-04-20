be68221f5694729c722ca429e166f451c6b6c8b2
krogers <orkrod98@gmail.com>
2023-04-20 13:11:11 -0600

    add ae, but error with loss compuation (becomes nan quickly)

--- model/network.py
+++ model/network.py
@@ -13,10 +13,40 @@ class GradSaver(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, gradients):
+        # old method using least squares 
+        # x, saver, sequentialOutput, = ctx.saved_tensors
+        # m = torch.linalg.lstsq(x, sequentialOutput).solution
+        # saver.grad = gradients.clone()
+        # return torch.matmul(gradients, torch.transpose(m, 0, 1)), None, None
+        
         x, saver, sequentialOutput, = ctx.saved_tensors
-        m = torch.linalg.lstsq(x, sequentialOutput).solution
-        saver.grad = gradients.clone()
-        return torch.matmul(gradients, torch.transpose(m, 0, 1)), None, None
+        # s = time.time()
+        t = 1e-9
+        x_T = torch.transpose(x, 0, 1)
+        I = torch.eye(x.shape[0]).to('cuda:1')
+        pinv = torch.mm(x_T, torch.inverse(torch.mm(x, x_T) + t * I)) 
+        # print(pinv.shape)
+        m = torch.mm(pinv, sequentialOutput) # torch.transpose(pinv, -1, 1)
+        # print(m.shape)
+
+
+        # print(x.shape)
+        # print(sequentialOutput.shape)
+        # print('saver dtype ', saver.dtype, saver.shape, saver.device)
+        # print('gradients dtype ', gradients.dtype, gradients.shape, gradients.device)
+        saver.grad = gradients.clone()#.cpu()
+        # print('in grad saver ', saver.grad)
+
+
+        # print(gradients.shape, m.shape)
+        # a = 
+    
+        # z = torch.mm(gradients, m)#, None, None
+        # z = torch.bmm(gradients, m)
+        # lst_sqs_sum += (time.time() -s )
+        z = torch.matmul(gradients, torch.transpose(m, 0, 1))#, None, None
+        # print('matmul result in grad saver: ', z)
+        return z, None, None
 
 
 
@@ -25,7 +55,7 @@ class TeacherNet(nn.Module):
     def __init__(self, total_image_pixel = 784):
         super(TeacherNet, self).__init__()
         self.fc1 = nn.Linear(total_image_pixel, 392)
-        self.distillable = torch.nn.Sequential(nn.Linear(392, 196), nn.ReLU(), nn.Linear(196, 98), nn.ReLU(), nn.Linear(98, 49), nn.ReLU())
+        self.distillable = torch.nn.Sequential(nn.Linear(392, 196), nn.ReLU(), nn.Linear(196, 98), nn.ReLU(), nn.Linear(98, 49), nn.ReLU()).to('cuda:1')
         self.fc3 = nn.Linear(49, 10)
 
         self.distill = False
@@ -33,7 +63,7 @@ class TeacherNet(nn.Module):
         self.distilledLayer = nn.Linear(392, 49)
 
     def forward(self, x):
-        x = torch.reshape(x, (x.shape[0], 28*28))
+        x = torch.reshape(x, (x.shape[0], 28*28)).to('cuda:1')
         
         x = F.relu(self.fc1(x))
         
@@ -59,7 +89,7 @@ class MNet(torch.nn.Module):
         # self.layers.requires_grad = False
         self.input_x = None
         self.layersOutput = []
-        self.saver = torch.ones((64,49), dtype = self.layers[1].weight.dtype, requires_grad=True) # check dims of this (batch, output of M) -- TODO
+        self.saver = torch.ones((64,49), dtype = self.layers[1].weight.dtype, requires_grad=True).to('cuda:1') # check dims of this (batch, output of M) -- TODO
         self.gradDiverge = GradSaver.apply
 
     def getLayersOutput(self, x):
@@ -89,12 +119,13 @@ class MNet(torch.nn.Module):
     #                 layer.weight = torch.nn.Parameter(layer.weight + deltaWeight * lr)
 
     def forward(self, x):
+        # print('in forward')
         x_clone = x.clone().detach()
         x_clone.requires_grad = False
-        self.sequentialOutput = self.getLayersOutput(x_clone)
+        self.sequentialOutput = self.getLayersOutput(x_clone).to('cuda:1')
         # self.saver = sequentialOutput.clone().detach()
         if self.saver.shape != self.sequentialOutput.shape:
-            self.saver = torch.ones(self.sequentialOutput.shape)
+            self.saver = torch.ones(self.sequentialOutput.shape).to('cuda:1')
         # print(self.sequentialOutput.requires_grad)
         # m = torch.linalg.lstsq(x_clone, self.sequentialOutput).solution.detach()
         # o = F.linear(x, torch.transpose(m, 0, 1))
@@ -121,14 +152,19 @@ class FastUpdateNet(torch.nn.Module):
             self.fc3 = teacherNet.fc3
         else:
             self.fc1 = nn.Linear(total_image_pixel, 392)
-            self.mNet = MNet(torch.nn.Sequential(nn.Linear(392, 196), nn.ReLU(), nn.Linear(196, 98), nn.ReLU(), nn.Linear(98, 49), nn.ReLU()))
+            self.mNet = MNet(torch.nn.Sequential(nn.Linear(392, 196), nn.ReLU(), nn.Linear(196, 98), nn.ReLU(), nn.Linear(98, 49), nn.ReLU()).to('cuda:1'))
             self.fc3 = nn.Linear(49, 10)
 
     def forward(self, x):
-        o_1 = torch.reshape(x, (x.shape[0], 28*28))
+        # print(x.device)
+        o_1 = torch.reshape(x, (x.shape[0], 28*28)).to('cuda:1')
+        # print(o_1.device)
         o_2 = F.relu(self.fc1(o_1))
+        # print(o_2.device)
         o_3 = self.mNet(o_2)
+        # print(o_3.device)
         o_4 = self.fc3(o_3)
+        # print(o_4.device)
         return F.log_softmax(o_4)
 
     def get_parameters(self):
@@ -177,6 +213,52 @@ class FastUpdateNetLarge(torch.nn.Module):
         ps.append(self.fc1.bias)
         ps.append(self.fc2.weight)
         ps.append(self.fc2.bias)
+        ps.append(self.fc3.weight)
+        ps.append(self.fc3.bias)
+        for p in mnet1Ps:
+            ps.append(p)
+        for p in mnet2Ps:
+            ps.append(p)
+        return ps
+    
+class Linear_AE(torch.nn.Module):
+    def __init__(self, teacherNet = None, total_image_pixel = 784, encoding_dim=10):
+        super(Linear_AE, self).__init__()
+        
+        self.fc1 = nn.Linear(total_image_pixel, 392)
+        self.mNet1 = MNet(torch.nn.Sequential(nn.Linear(392, 196), nn.ReLU(), nn.Linear(196, 98), nn.ReLU(), nn.Linear(98, 49), nn.ReLU()).to('cuda:1'))
+        self.mNet2 = MNet(torch.nn.Sequential(nn.Linear(49, 98), nn.ReLU(), nn.Linear(98, 196), nn.ReLU(), nn.Linear(196, 392), nn.ReLU()).to('cuda:1'))
+        self.fc3 = nn.Linear(49, 10)
+
+        self.fc5 = nn.Linear(10,49)
+        self.fc8 = nn.Linear(392, total_image_pixel)
+
+    def forward(self, x):
+
+        x = torch.reshape(x, (x.shape[0], 28*28)).to('cuda:1')
+
+        # encoder
+        o_1 = F.relu(self.fc1(x))
+        o_4 = self.mNet1(o_1)
+        o_5 = self.fc3(o_4)
+
+        self.encoded_x = o_5 # in case we want to do any sort of clustering analysis
+
+        # decoder (reverse encoder layers)
+        o_6 = F.relu(self.fc5(o_5))
+        o_9 = self.mNet2(o_6)
+        o_10 = self.fc8(o_9)
+
+        y = torch.reshape(o_10, (o_10.shape[0], 28, 28))
+        y = y.squeeze(1)
+        return o_10, y
+
+    def get_parameters(self):
+        ps = []
+        mnet1Ps = self.mNet1.get_parameters()
+        mnet2Ps = self.mNet2.get_parameters()
+        ps.append(self.fc1.weight)
+        ps.append(self.fc1.bias)
         ps.append(self.fc3.weight)
         ps.append(self.fc3.bias)
         for p in mnet1Ps:
